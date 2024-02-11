63247f44180e47cc0d5224a79fe6e0d4d3ea1640
Itallo Ramon <irvp@ic.ufal.br>
2024-02-11 12:41:16 -0300

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+# Load the Rails application.
+require_relative "application"
+
+# Load the app's custom environment variables here, so that they are loaded before environments/*.rb
+environment_variables = File.join(Rails.root, 'config', 'environment_variables.rb')
+load(environment_variables) if File.exist?(environment_variables)
+
+# Initialize the Rails application.
+Rails.application.initialize!
