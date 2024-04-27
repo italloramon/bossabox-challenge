eeb7a922c039c56922565d338f45953c68d5557b
italloramon <itallostudy@gmail.com>
2024-04-27 18:23:36 -0300

    feat: add CRUD operations

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,36 @@
+class ToolsController < ApplicationController
+  def index
+    render json: Tool.all
+  end
+
+  def show
+    render json: Tool.find(params[:id])
+  end
+
+  def create
+    tool = Tool.create!(tool_params)
+    render json: tool, status: :created
+  end
+
+  def edit
+    @tool = Tool.find(params[:id])
+  end
+
+  def update
+    tool = Tool.find(params[:id])
+    tool.update!(tool)
+    render json: tool
+  end
+
+  def destroy
+    Tool.find(params[:id]).destroy
+    head :no_content
+  end
+
+  private
+
+  def tool_params
+    params.permit(:title, :link, :description, :tags)
+  end
+
+end
