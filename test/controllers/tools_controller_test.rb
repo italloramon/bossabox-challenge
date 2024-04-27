260841737b4ee0bf318773a3ec7313cd811e6465
italloramon <itallostudy@gmail.com>
2024-04-27 19:00:33 -0300

    test: add unit test for tools_controller

--- test/controllers/tools_controller_test.rb
+++ test/controllers/tools_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class ToolsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @tool = tools(:one)
+  end
+
+  test 'should get index' do
+    get tools_url, as: :json
+    assert_response :success
+  end
+
+  test 'should create tool' do
+    assert_difference('Tool.count') do
+      post tools_url, params: { title: 'New Tool', link: 'https://newtool.com', description: 'New tool description', tags: ['tag1', 'tag2'] }, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test 'should show tool' do
+    get tool_url(@tool), as: :json
+    assert_response :success
+  end
+
+  test 'should update tool' do
+    patch tool_url(@tool), params: { title: 'Updated Tool', link: 'https://updatedtool.com', description: 'Updated tool description', tags: ['tag3', 'tag4'] }, as: :json
+    assert_response :success
+  end
+
+  test 'should destroy tool' do
+    assert_difference('Tool.count', -1) do
+      delete tool_url(@tool), as: :json
+    end
+
+    assert_response :no_content
+  end
 end
